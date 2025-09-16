b5292baec234f29823189333577033ca551b0fed
alvss-st-call <Juliaalves68305@gmail.com>
2025-09-16 09:12:41 -0300

    Add files via upload

---
+++ temperatura.py
@@ -0,0 +1,10 @@
+temperatura = float(input("digite a temperatura:"))
+
+if temperatura >= 30:
+    print ("quente")
+elif temperatura >=20:
+    print("agradavel")
+elif temperatura >=10:
+    print("frio")
+else:
+    print("muito frio")
